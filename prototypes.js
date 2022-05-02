a47690e9deb19ffd63207b687eae1d15c9a73ffb
Luis <iluis.06@outlook.com>
2022-05-02 18:02:12 -0500

    prototypes

---
+++ prototypes.js
@@ -0,0 +1,24 @@
+//Función constructura donde asignamos a los métodos
+//al prototypo, no  a la función como tal.
+
+function Animal(nombre, genero) {
+	this.nombre = nombre;
+	this.genero = genero;
+
+}
+
+//Métodos agregados al prototipo de la función constructora
+Animal.prototype.sonar = function () {
+	console.log("Hago sonidos por que estoy vivo.");
+}
+
+Animal.prototype.saludar = function () {
+	console.log(`Hola me llamo ${this.nombre}`);
+}
+
+const snoopy = new Animal("Snoopy", "Macho");
+
+console.log(snoopy);
+
+snoopy.sonar();
+snoopy.saludar();
